5ed7952dbeb13ca72272fb2aa48fb58e523bf4e9
Rafael Silva <73067778+RafaeISilva@users.noreply.github.com>
2022-07-21 18:37:38 -0300

    started adding custom questions
    added a way of adding custom questions to the game, but deleting them was not as successful :'(

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from random import randrange
 
 from kivy.app import App
 from kivy.core.window import Window
+from kivy.factory import Factory
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import Screen, ScreenManager
@@ -13,6 +14,7 @@ from utils import (
     questions_hard,
     questions_medium,
     reload_game,
+    save_question,
     signs_rng,
     skip,
 )
@@ -94,6 +96,72 @@ class LoseScreen(Screen):
 
 class WinScreen(Screen):
     pass
+
+
+class AddQuestionScreen(Screen):
+    title_add = ObjectProperty(None)
+    option1_add = ObjectProperty(None)
+    option2_add = ObjectProperty(None)
+    option3_add = ObjectProperty(None)
+    option4_add = ObjectProperty(None)
+
+    difficulty = None
+
+    def select_difficulty(self, difficulty):
+        self.difficulty = difficulty
+
+    def confirm(self):
+        title = self.title_add.text
+        options = [
+            self.option1_add,
+            self.option2_add,
+            self.option3_add,
+            self.option4_add,
+        ]
+        if title != "" and self.difficulty != None:
+            for i, option in enumerate(options):
+                if option.text == "":
+                    Factory.WarningBad().open()
+                    return
+                else:
+                    options[i] = option.text
+            save_question(title, options, self.difficulty)
+            self.title_add.text = ""
+            self.option1_add.text = ""
+            self.option2_add.text = ""
+            self.option3_add.text = ""
+            self.option4_add.text = ""
+            Factory.WarningGood().open()
+        else:
+            Factory.WarningBad().open()
+
+
+class RemoveQuestionScreen(Screen):
+    rq_title = ObjectProperty(None)
+    rq_option1 = ObjectProperty(None)
+    rq_option2 = ObjectProperty(None)
+    rq_option3 = ObjectProperty(None)
+    rq_option4 = ObjectProperty(None)
+
+    questions = questions_easy[:] + questions_medium[:] + questions_hard[:]
+    index = 0
+    index_limit = len(questions) - 1
+
+    def next_question(self):
+        question = self.questions[self.index]
+        option_indexes = [0, 1, 2, 3]
+        option_indexes.remove(question.answer - 1)
+
+        self.rq_title.text = question.title
+        self.rq_option1.text = question.options[question.answer - 1]
+        self.rq_option2.text = question.options[option_indexes[0]]
+        self.rq_option3.text = question.options[option_indexes[1]]
+        self.rq_option4.text = question.options[option_indexes[2]]
+
+        if self.index < self.index_limit:
+            self.index += 1
+        else:
+            self.index = 0
 
 
 class ShowDoMilhao(App):

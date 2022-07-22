e294f111bd8fa00e1f57eb3f451db87a90169313
Rafael Silva <73067778+RafaeISilva@users.noreply.github.com>
2022-07-22 12:41:02 -0300

    added custom questions support

--- pt-br/utils.py
+++ pt-br/utils.py
@@ -18,35 +18,76 @@ class Question:
                 self.answer = i + 1
 
 
-with open("questions.json", "r") as f:
-    questions = json.loads(f.read())
+class NoQuestions(Exception):
+    pass
 
-questions_easy = []
-questions_medium = []
-questions_hard = []
 
-for i in questions:
-    if questions[i]["difficulty"] == "easy":
-        questions_easy.append(
-            Question(
-                questions[i]["title"],
-                questions[i]["options"],
-            )
-        )
-    if questions[i]["difficulty"] == "medium":
-        questions_medium.append(
-            Question(
-                questions[i]["title"],
-                questions[i]["options"],
-            )
-        )
-    if questions[i]["difficulty"] == "hard":
-        questions_hard.append(
-            Question(
-                questions[i]["title"],
-                questions[i]["options"],
-            )
-        )
+def format_index(num):
+    if num < 10:
+        return f"00{num}"
+    elif num < 100:
+        return f"0{num}"
+    else:
+        return f"{num}"
+
+
+def save_question(title, options, difficulty):
+    options[0] = f"*{options[0]}"
+
+    with open("custom_questions.json", "r") as f:
+        custom_questions = json.loads(f.read())
+
+    index = format_index(len(custom_questions) + 1)
+    custom_questions[index] = {
+        "title": title,
+        "options": options,
+        "difficulty": difficulty,
+    }
+    with open("custom_questions.json", "w") as f:
+        f.write(json.dumps(custom_questions, indent=4))
+
+
+# formats custom questions json if wrong
+def validate_dict(dict_):
+    output = {}
+    difficulties = ["easy", "medium", "hard"]
+    for i, key in enumerate(dict_):
+        if format_index(i + 1) != key:
+            index = format_index(i + 1)
+        else:
+            index = key
+
+        output[index] = {}
+        try:
+            output[index]["title"] = dict_[key]["title"]
+        except KeyError:
+            output[index]["title"] = ""
+        try:
+            output[index]["options"] = dict_[key]["options"]
+        except KeyError:
+            output[index]["options"] = []
+        try:
+            output[index]["difficulty"] = dict_[key]["difficulty"]
+        except KeyError:
+            output[index]["difficulty"] = "easy"
+
+        output2 = len(output[index]["options"])
+
+        if output2 < 4:
+            for i in range(4 - output2):
+                output[index]["options"].append("")
+        elif output2 > 4:
+            for i in range(output2 - 4):
+                output[index]["options"].pop()
+
+        if not output[index]["options"][0].startswith("*"):
+            output[index]["options"][0] = f"*{output[index]['options'][0]}"
+
+        if output[index]["difficulty"] not in difficulties:
+            output[index]["difficulty"] = "easy"
+
+    return output
+
 
 # checks if selected option is right or wrong
 def check_answer(obj, option):
@@ -149,3 +190,73 @@ def reload_options(obj):
     for option in options:
         if option.disabled:
             option.disabled = False
+
+
+try:
+    with open("custom_questions.json", "r") as f:
+        questions = json.loads(f.read())
+    if questions == {}:
+        raise NoQuestions
+    custom = True
+except (NoQuestions, FileNotFoundError, json.decoder.JSONDecodeError) as error:
+    if type(error) == FileNotFoundError or type(error) == json.decoder.JSONDecodeError:
+        with open("custom_questions.json", "w") as f:
+            f.write(json.dumps({}))
+
+    with open("questions.json", "r") as f:
+        questions = json.loads(f.read())
+    custom = False
+
+questions_easy = []
+questions_medium = []
+questions_hard = []
+
+for i in questions:
+    if questions[i]["difficulty"] == "easy":
+        questions_easy.append(
+            Question(
+                questions[i]["title"],
+                questions[i]["options"],
+            )
+        )
+    if questions[i]["difficulty"] == "medium":
+        questions_medium.append(
+            Question(
+                questions[i]["title"],
+                questions[i]["options"],
+            )
+        )
+    if questions[i]["difficulty"] == "hard":
+        questions_hard.append(
+            Question(
+                questions[i]["title"],
+                questions[i]["options"],
+            )
+        )
+
+# for the game to work at least 24 questions are needed
+total = len(questions_easy) + len(questions_medium) + len(questions_hard)
+if custom and total < 24:
+    with open("questions.json") as f:
+        filler_questions = json.loads(f.read())
+    # 3 easy questions + 3 skips
+    if len(questions_easy) < 6:
+        quantity = 6 - len(questions_easy)
+        for i in range(quantity):
+            index = randint(1, 150)
+            q = filler_questions[format_index(index)]
+            questions_easy.append(Question(q["title"], q["options"]))
+    # 7 medium questions + 3 skips
+    if len(questions_medium) < 10:
+        quantity = 10 - len(questions_medium)
+        for i in range(quantity):
+            index = randint(151, 500)
+            q = filler_questions[format_index(index)]
+            questions_medium.append(Question(q["title"], q["options"]))
+    # 8 hard questions + 3 skips
+    if len(questions_hard) < 8:
+        quantity = 10 - len(questions_hard)
+        for i in range(quantity):
+            index = randint(501, 750)
+            q = filler_questions[format_index(index)]
+            questions_hard.append(Question(q["title"], q["options"]))

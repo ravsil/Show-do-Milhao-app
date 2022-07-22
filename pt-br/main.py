15066a38f9aab03ba835299b20b413d4f3a332ed
Rafael Silva <73067778+RafaeISilva@users.noreply.github.com>
2022-07-22 12:40:24 -0300

    added custom questions support

--- pt-br/main.py
+++ pt-br/main.py
@@ -1,7 +1,9 @@
+import json
 from random import randrange
 
 from kivy.app import App
 from kivy.core.window import Window
+from kivy.factory import Factory
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import Screen, ScreenManager
@@ -13,8 +15,10 @@ from utils import (
     questions_hard,
     questions_medium,
     reload_game,
+    save_question,
     signs_rng,
     skip,
+    validate_dict,
 )
 
 
@@ -94,6 +98,65 @@ class LoseScreen(Screen):
 
 class WinScreen(Screen):
     pass
+
+
+class AddQuestionScreen(Screen):
+    title_add = ObjectProperty(None)
+    option1_add = ObjectProperty(None)
+    option2_add = ObjectProperty(None)
+    option3_add = ObjectProperty(None)
+    option4_add = ObjectProperty(None)
+
+    difficulty = None
+    difficulty_trans = {"fácil": "easy", "médio": "medium", "difícil": "hard"}
+
+    def select_difficulty(self, difficulty):
+        self.difficulty = self.difficulty_trans[difficulty]
+
+    def confirm(self):
+        title = self.title_add.text
+        options = [
+            self.option1_add,
+            self.option2_add,
+            self.option3_add,
+            self.option4_add,
+        ]
+        if title != "" and self.difficulty != None:
+            for i, option in enumerate(options):
+                if option.text == "":
+                    Factory.WarningBad().open()
+                    return
+                else:
+                    options[i] = option.text
+            save_question(title, options, self.difficulty)
+            self.title_add.text = ""
+            self.option1_add.text = ""
+            self.option2_add.text = ""
+            self.option3_add.text = ""
+            self.option4_add.text = ""
+            Factory.WarningGood().open()
+        else:
+            Factory.WarningBad().open()
+
+
+class AdvancedScreen(Screen):
+    custom_json = ObjectProperty(None)
+
+    def update_text(self):
+        with open("custom_questions.json", "r") as f:
+            tmp = f.read()
+        self.custom_json.text = tmp
+
+    def save_custom(self):
+        try:
+            tmp = validate_dict(json.loads(self.custom_json.text))
+            with open("custom_questions.json", "w") as f:
+                f.write(json.dumps(tmp, indent=4))
+            Factory.WarningGood().open()
+            self.custom_json.text = ""
+            self.manager.current = "add_question"
+        except json.decoder.JSONDecodeError:
+            Factory.WarningBad().open()
 
 
 class ShowDoMilhao(App):
